20187fe339e3a2d1cf3cee83360e02a8e0a1622e
Aleksey Popov <oluqom@proton.me>
2023-07-14 15:08:55 +0700

    feat: update Home page

--- src/pages/Home/ui/Page/Page.tsx
+++ src/pages/Home/ui/Page/Page.tsx
@@ -1,80 +1,56 @@
 import { FC } from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
 
-const data = {
-  coord: {
-    lon: 10.99,
-    lat: 44.34,
-  },
-  weather: [
-    {
-      id: 501,
-      main: "Rain",
-      description: "moderate rain",
-      icon: "10d",
-    },
-  ],
-  base: "stations",
-  main: {
-    temp: 298.48,
-    feels_like: 298.74,
-    temp_min: 297.56,
-    temp_max: 300.05,
-    pressure: 1015,
-    humidity: 64,
-    sea_level: 1015,
-    grnd_level: 933,
-  },
-  visibility: 10000,
-  wind: {
-    speed: 0.62,
-    deg: 349,
-    gust: 1.18,
-  },
-  rain: {
-    "1h": 3.16,
-  },
-  clouds: {
-    all: 100,
-  },
-  dt: 1661870592,
-  sys: {
-    type: 2,
-    id: 2075663,
-    country: "IT",
-    sunrise: 1661834187,
-    sunset: 1661882248,
-  },
-  timezone: 7200,
-  id: 3163858,
-  name: "Zocca",
-  cod: 200,
+const weather = {
+  city: "London", // Choose your city
+  api: "04e0ff6a5bb18c2a686c6becd886c64e",
+  units: "metric", // "imperial" or "metric"
+  lang: "en", // "en" or "ru"
 };
 
 const Home: FC = () => {
+  const url = `https://api.openweathermap.org/data/1.5/weather?q=${weather.city}&appid=${weather.api}&units=${weather.units}&lang=${weather.lang}`;
+  const { isLoading, error, data } = useQuery("weatherData", () =>
+    axios.get(url).then((res) => res.data),
+  );
+
+  if (isLoading)
+    return (
+      <>
+        <div className="container flex justify-center py-5">
+          <h2 className="text-neutral-dark text-2xl font-bold">Loading...</h2>
+        </div>
+      </>
+    );
+
+  if (error)
+    return (
+      <>
+        <div className="container flex justify-center py-5">
+          <h2 className="text-neutral-dark text-2xl font-bold">
+            {"Error: " + error}
+          </h2>
+        </div>
+      </>
+    );
+
   return (
     <>
       <section>
-        <div className="container py-5">
-          <div className="flex items-center">
-            <div className="input-group justify-start">
-              <input className="input" type="text" placeholder="Search city" />
-              <button className="btn-neutral btn">Search</button>
-            </div>
-            <div className="input-group justify-end">
-              <button className="btn-neutral btn">Metric: °C, m/s</button>
-              <button className="btn-neutral btn">Imperial: °F, mph</button>
-            </div>
+        <div className="container flex flex-col items-center justify-center py-5">
+          <div className="input-group flex w-full justify-center">
+            <input className="input-primary input" />
+            <button className="btn-accent btn">Search</button>
           </div>
-
           <div className="flex gap-10 py-10">
             <div className="flex w-max flex-col justify-between">
               <div className="mb-2">
-                <p className="mb-1 text-xl text-neutral">Jul 2, 10:30am</p>
                 <h2 className="text-neutral-dark mb-3 text-2xl font-bold">
                   {data.name}, {data.sys.country}
                 </h2>
                 <h1 className="text-7xl font-light text-neutral">
-                  {data.main.temp}
+                  {Math.floor(data.main.temp)} °
                 </h1>
               </div>
               <div>
@@ -82,28 +58,23 @@ const Home: FC = () => {
                   <p className="w-max rounded bg-neutral px-2 py-1 text-lg font-medium text-base-100">
                     {data.weather[0].main}
                   </p>
-                  <p className="w-max rounded bg-neutral px-2 py-1 text-lg font-medium text-base-100">
-                    Rain
-                  </p>
                 </div>
                 <p className="text-neutral-dark mb-2 text-lg font-medium">
                   Feels like {data.main.feels_like}.{" "}
                   {data.weather[0].description}
                 </p>
                 <ul className="flex w-[17rem] flex-wrap gap-x-2 border-l-2 border-neutral px-3">
-                  <li className="text-base text-neutral">2.0m/s SE</li>
                   <li className="text-base text-neutral">
                     {data.main.pressure}hPa
                   </li>
-                  <li className="text-base text-neutral">Humidity: 60%</li>
-                  <li className="text-base text-neutral">UV: 4</li>
-                  <li className="text-base text-neutral">Dew point: 12°C</li>
-                  <li className="text-base text-neutral">Visibility: 10.0km</li>
+                  <li className="text-base text-neutral">
+                    Humidity: {data.main.humidity}%
+                  </li>
+                  <li className="text-base text-neutral">
+                    Visibility: {Math.floor(data.visibility / 1000)}km
+                  </li>
                 </ul>
               </div>
-            </div>
-            <div className="h-80 flex-1 rounded bg-neutral p-5">
-              <p className="text-3xl font-bold text-base-100">Map</p>
             </div>
           </div>
         </div>
